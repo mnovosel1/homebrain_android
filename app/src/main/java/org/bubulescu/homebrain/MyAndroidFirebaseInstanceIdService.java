cfe29dcd3877beee9f123e5a90d4ba84b12cd17c
Marijo Novosel <marijo.novosel@gmail.com>
2017-09-05 14:02:18 +0200

    server registration

--- app/src/main/java/org/bubulescu/homebrain/MyAndroidFirebaseInstanceIdService.java
+++ app/src/main/java/org/bubulescu/homebrain/MyAndroidFirebaseInstanceIdService.java
@@ -4,6 +4,12 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MyAndroidFirebaseInstanceIdService extends FirebaseInstanceIdService {
 
     private static final String TAG = "MyAndroidFCMIIDService";
@@ -13,9 +19,23 @@ public class MyAndroidFirebaseInstanceIdService extends FirebaseInstanceIdServic
         //Get hold of the registration token
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         //Log the token
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        //Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
     }
     private void sendRegistrationToServer(String token) {
-        //Implement this method if you want to store the token on your server
+        try {
+            URL url = new URL("http://hb.bubulescu.org/reg/?id=" + token);
+            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+            httpCon.setDoOutput(true);
+            httpCon.getInputStream();
+            Log.d(TAG, "Registered token to server: " + token);
+        }
+        catch (MalformedURLException ex) {
+            Log.d(TAG, Log.getStackTraceString(ex));
+        }
+        catch (IOException ex) {
+            Log.d(TAG, Log.getStackTraceString(ex));
+        }
     }
 }
