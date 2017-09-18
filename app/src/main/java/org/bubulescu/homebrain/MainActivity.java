d3f80d63d7b3e631c6d288c2db5cbf2b3438f8aa
Marijo Novosel <marijo.novosel@gmail.com>
2017-09-18 13:19:05 +0200

    added tts and improved page loading function..

--- app/src/main/java/org/bubulescu/homebrain/MainActivity.java
+++ app/src/main/java/org/bubulescu/homebrain/MainActivity.java
@@ -1,24 +1,42 @@
 package org.bubulescu.homebrain;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView wb;
+    private TextToSpeech tts;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.UK);
+                }
+            }
+        });
+
         wb = (WebView) findViewById(R.id.webView);
+        wb.addJavascriptInterface(new WebAppInterface(this), "Android");
         wb.getSettings().setLoadWithOverviewMode(true);
         wb.getSettings().setUseWideViewPort(true);
 
@@ -37,6 +55,47 @@ public class MainActivity extends AppCompatActivity {
             wb.goBack();
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public class WebAppInterface
+    {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c)
+        {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void toast(String toast)
+        {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+
+        @JavascriptInterface
+        public void speak (String toSpeak)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ttsGreater21(toSpeak);
+            } else {
+                ttsUnder20(toSpeak);
+            }
+        }
+
+        @SuppressWarnings("deprecation")
+        private void ttsUnder20(String text) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+        }
+
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        private void ttsGreater21(String text) {
+            String utteranceId=this.hashCode() + "";
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
         }
     }
 }
