fa350a86675605572ce41dd697e10056c793b448
Marijo Novosel <marijo.novosel@gmail.com>
2017-09-23 14:21:01 +0200

    data message implemented

--- app/src/main/java/org/bubulescu/homebrain/MyAndroidFirebaseMsgService.java
+++ app/src/main/java/org/bubulescu/homebrain/MyAndroidFirebaseMsgService.java
@@ -2,23 +2,52 @@ package org.bubulescu.homebrain;
 
 import android.app.NotificationManager;
 import android.content.Context;
-import android.graphics.Color;
-import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.webkit.WebView;
+import android.widget.Toast;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class MyAndroidFirebaseMsgService extends FirebaseMessagingService {
+public class MyAndroidFirebaseMsgService extends FirebaseMessagingService
+{
     private static final String TAG = "MyAndroidFCMService";
+    private String msg;
+    private WebView wb;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        //Log data to Log Cat
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        //create notification
-        createNotification(remoteMessage.getNotification().getBody());
+
+        msg = remoteMessage.getData().get("msg");
+
+        //notification message
+        if(msg == null || msg.trim().isEmpty())
+        {
+            msg = remoteMessage.getNotification().getBody();
+
+            Log.d(TAG, "From: " + remoteMessage.getFrom());
+            Log.d(TAG, "Notification Message: " + msg);
+        }
+        //data message
+        else
+        {
+            Log.d(TAG, "From: " + remoteMessage.getFrom());
+            Log.d(TAG, "DATA Message: " + msg);
+        }
+
+
+        //createNotification(msg);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void createNotification(String messageBody) {
@@ -35,5 +64,6 @@ public class MyAndroidFirebaseMsgService extends FirebaseMessagingService {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, mNotificationBuilder.build());
+
     }
 }
