68d9d07479f1f9745dab70820c201f7fbf27d63e
Marijo Novosel <marijo.novosel@gmail.com>
2017-10-09 10:48:47 +0200

    notification onclick opens app (if not oppened) and autoclears

--- app/src/main/java/org/bubulescu/homebrain/MyAndroidFirebaseMsgService.java
+++ app/src/main/java/org/bubulescu/homebrain/MyAndroidFirebaseMsgService.java
@@ -1,8 +1,10 @@
 package org.bubulescu.homebrain;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
@@ -97,10 +99,16 @@ public class MyAndroidFirebaseMsgService extends FirebaseMessagingService
 
     private void createNotification(String messageTitle, String messageBody) {
 
+        PackageManager pm = getPackageManager();
+        Intent launchIntent = pm.getLaunchIntentForPackage("org.bubulescu.homebrain");
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
+
         NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_stat_home)
                 .setContentTitle(messageTitle)
-                .setContentText(messageBody);
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setContentIntent(contentIntent);
 
         switch (messageTitle)
         {
@@ -124,8 +132,7 @@ public class MyAndroidFirebaseMsgService extends FirebaseMessagingService
                 mNotificationBuilder.setSound(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.notify));
         }
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, mNotificationBuilder.build());
 
