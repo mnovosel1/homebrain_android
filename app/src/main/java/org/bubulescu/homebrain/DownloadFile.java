857a98358eccb14f452e32d8722fd0a9a0f32ca2
Marijo Novosel <marijo.novosel@gmail.com>
2017-10-25 16:48:24 +0200

    DownloadFile external filepath

--- app/src/main/java/org/bubulescu/homebrain/DownloadFile.java
+++ app/src/main/java/org/bubulescu/homebrain/DownloadFile.java
@@ -3,6 +3,9 @@ package org.bubulescu.homebrain;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Environment;
+
+import java.io.File;
 
 
 public class DownloadFile {
@@ -16,18 +19,19 @@ public class DownloadFile {
 
     public DownloadFile(Context context, String downloadURL, String dlFilename) {
 
+        File dir = new File(Environment.getExternalStorageDirectory() + "/hbrain");
+        if ( !dir.exists() ) dir.mkdirs();
 
         //downloadURL = "https://hbr.bubulescu.org:9343/upd";
 
         DownloadManager downloadmanager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         Uri uri = Uri.parse(downloadURL);
+
         DownloadManager.Request request = new DownloadManager.Request(uri);
-
-
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationUri(Uri.parse("file://hbrain/" + dlFilename ));
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
+               .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+               .setDestinationInExternalPublicDir("/homebrain", dlFilename);
 
         downloadmanager.enqueue(request);
-
     }
 }
